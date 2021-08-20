93e6d46c4e7cce6f7253836bc1a497f7366a84c2
Kevin Garcia <54457637+kevinShogun@users.noreply.github.com>
2021-08-20 16:20:48 -0600

    Create Store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
 import { AppRouter } from './components/routers/AppRouter'
+import { store } from './components/store/store';
 import TestGridView from './view/testGrid/TestGrid';
 
 const App = () => {
   return (
+    <Provider store={store}>
         <TestGridView />
+    </Provider>
   );
 }
 
